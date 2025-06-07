e7faa2f00c7d6577301123184ee7f3cb3a895234
K Mani Chandy <kmchandy@gmail.com>
2025-06-07 14:34:18 -0700

    better comments

--- lang_graph_examples/sequence_basic.py
+++ lang_graph_examples/sequence_basic.py
@@ -1,12 +1,5 @@
 """
-This module is a simple example of how to use LangGraph with OpenAI's GPT-3.5-turbo model.
-The graph in this module has only two nodes and one edge.
-The module has 5 steps:
-1. Set up OpenAI API client.
-2. Define a shared state structure using TypedDict.
-3. Specify the functions that will be executed by nodes in the graph.
-4. Build the state graph with nodes and edges.
-5. Run the graph with an input.
+This module is an example of a LangGraph with two nodes in sequence.
 """
 
 import sys
@@ -40,7 +33,7 @@ llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.3)
 # ----------------------------------------------
 
 
-class GreetState(TypedDict):
+class MyState(TypedDict):
     name: str
     greeting: str
     compliment: str
@@ -51,37 +44,71 @@ class GreetState(TypedDict):
 # ----------------------------------------------
 
 
-def greet_agent(state: GreetState) -> dict:
+def greet_agent(state: MyState) -> dict:
     name = state["name"]
     response = llm.invoke(
-        f"Say two kind short sentences about the name {name}.\n")
+        f"Say a single kind short sentence about the name {name}.\n")
+    # Put the content of the response into the state of the agent.
+    # state["greeting"] becomes response.content.
+    # The response is not printed.
     return {"greeting": response.content}
 
 
-def compliment_agent(state: GreetState) -> dict:
+def compliment_agent(state: MyState) -> dict:
     greeting = state["greeting"]
-    response = llm.invoke(f"{greeting} Say one motivational sentence.")
+    response = llm.invoke(
+        f"Say one motivational sentence based on {greeting}.")
+    # Put the content of the response into the state of the agent.
+    # state["compliment"] becomes response.content
     return {"compliment": response.content}
 
 
 # ---------------------------------------------
 # Step 4: Build the graph
 # ----------------------------------------------
-builder = StateGraph(GreetState)
-# Add nodes to the graph
+
+# 4.1 Create builder
+builder = StateGraph(MyState)
+
+# 4.2 Add nodes to the graph.
+# Give a name to the node and specify the function
+# that will be executed by the node.
+# Here we create two nodes called "greet_node" and "compliment_node".
 builder.add_node("greet_node", greet_agent)
 builder.add_node("compliment_node", compliment_agent)
-# Add edges to the graph
+
+# 4.3 Define the edges between nodes of the graph.
+# In this case, the graph has a single edge.
+# The edge is from "greet_node" to "compliment_node".
 builder.add_edge("greet_node", "compliment_node")
-# Set the entry and finish points for the graph
+
+# 4.4 Specify the entry and finish points of the graph.
 builder.set_entry_point("greet_node")
 builder.set_finish_point("compliment_node")
 
+# 4.5 Compile the graph
 graph = builder.compile()
 
 # ---------------------------------------------
 # Step 5: EXECUTE SCRIPT DIRECTLY
 # ----------------------------------------------
+
+'''
+Execution of the graph is as follows.
+The graph is invoked with an input that is a dictionary.
+The keys of the input dict must match the keys in MyState.
+
+The entry point node, greet_node, is executed with a state
+specified by the dict, {"name": input_name}, in the invoke.
+So, when greet_node is executed state["name"] is set to input_name,
+and state["greeting"] and state["compliment"] are unspecified.
+
+greet_node puts the result of the LLM invocation into state["greeting"].
+Then execution of compliment_node begins with a state with values for
+the keys "name" and "greeting", and state["compliment"] is unspecified.
+compliment_node puts the result of the LLM invocation into state["compliment"].
+Finally, the graph returns a state with values for all three keys.
+'''
 if __name__ == "__main__":
     '''Example Usage
     python3 LangGraph_Simple_Two_nodes_Example.py "Violet"
@@ -92,6 +119,8 @@ if __name__ == "__main__":
     # Get input_name from command line arguments
     input_name = sys.argv[1]
 
-    # Run graph with the input and print result.
+    # Execute the graph. The input to the graph is a dict and pretty print
+    # the result of the graph execution.
     result = graph.invoke({"name": input_name})
-    print(result)
+    import pprint
+    pprint.pprint(result)
