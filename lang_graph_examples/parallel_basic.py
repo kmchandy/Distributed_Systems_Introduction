d75623ecfc54300acc428d4da6db65544ee40eef
K Mani Chandy <kmchandy@gmail.com>
2025-06-18 10:35:50 -0700

    changed function to agent

--- lang_graph_examples/parallel_basic.py
+++ lang_graph_examples/parallel_basic.py
@@ -31,7 +31,7 @@ llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
 # ---------------------------------------------
 # Step 2: Define the shared state structure
-# This structure is often a TypedDict.
+# The state is a TypedDict.
 # ----------------------------------------------
 
 
@@ -44,31 +44,35 @@ class State(TypedDict):
 
 # ---------------------------------------------
 # Step 3: Specify the functions that are executed
-# by nodes in the graph
+# by nodes in the graph.
+# The functions return a dict where the keys are
+# also keys of State.
 # ----------------------------------------------
 
 
-def greet_agent(state: State) -> dict:
+def greet_function(state: State) -> dict:
     '''
     Reads state['name'] and assigns value to state['greeting'].
 
     '''
     prompt = f"Say a short sentence about {state['name']}."
     response = llm.invoke(prompt)
+    # state["greeting"] becomes response.content.
     return {"greeting": response.content}
 
 
-def topic_agent(state: State) -> dict:
+def topic_function(state: State) -> dict:
     '''
     Reads state['topic'] and assigns value to state['fact'].
 
     '''
     prompt = f"Tell me a short fact about {state['topic']}."
     response = llm.invoke(prompt)
+    # state["fact"] becomes response.content.
     return {"fact": response.content}
 
 
-def merge_agent(state: State) -> dict:
+def merge_function(state: State) -> dict:
     '''
     Reads state['greeting'] and state['fact'] and 
     assigns value to state['summary'].
@@ -76,6 +80,7 @@ def merge_agent(state: State) -> dict:
     '''
     prompt = f"Make a joke about {state['greeting']} and {state['fact']}"
     response = llm.invoke(prompt)
+    # state["summary"] becomes response.content.
     return {"summary": response.content}
 
 
@@ -93,9 +98,9 @@ builder = StateGraph(State)
 # merge_node, we create an entry node that feeds both
 # greet_node and topic_node. The entry node is a no-op.
 builder.add_node("entry_node", lambda x: x)
-builder.add_node("greet_node", greet_agent)
-builder.add_node("topic_node", topic_agent)
-builder.add_node("merge_node", merge_agent)
+builder.add_node("greet_node", greet_function)
+builder.add_node("topic_node", topic_function)
+builder.add_node("merge_node", merge_function)
 
 # 4.3 Specify the edges between nodes of the graph.
 builder.add_edge("entry_node", "greet_node")
