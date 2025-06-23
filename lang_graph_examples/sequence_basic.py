348f22875ed5a9cd66aa7070d7a927fa6705b8e5
K Mani Chandy <kmchandy@gmail.com>
2025-06-22 18:38:47 -0700

    added LangChain lm = ChatOpenAI(model=gpt-3.5-turbo, temperature=0.3)

--- lang_graph_examples/sequence_basic.py
+++ lang_graph_examples/sequence_basic.py
@@ -29,8 +29,6 @@ if not api_key:
     raise ValueError("No API key found. Please check your .env file.")
 
 # Create an OpenAI client
-client = OpenAI(api_key=api_key)
-
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.3)
 
 # ---------------------------------------------
